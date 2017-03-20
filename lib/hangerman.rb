ba6fc2f4bc411d829948324f461a9c70b65f07a6
Chris Eady <chris.eady1@gmail.com>
2017-03-20 17:07:16 -0500

    Add Player class with guess_letter method.

--- lib/hangerman.rb
+++ lib/hangerman.rb
@@ -1,5 +1,6 @@
 require "hangerman/version"
 require "hangerman/dictionary"
+require "hangerman/player"
 
 module Hangerman
   # Your code goes here...
---
+++ lib/hangerman/player.rb
@@ -0,0 +1,25 @@
+class Player
+
+  def guess_letter
+    input = ''
+    loop do
+      puts "Enter a letter:"
+      print ">> "
+      input = $stdin.gets.chomp
+      break if valid_letter?(input)
+    end
+    input
+  end
+
+  private
+
+  def valid_letter?(input)
+    if input =~ /^[A-Za-z]$/
+      true
+    else
+      puts "Invalid input!"
+      false
+    end
+  end
+
+end
