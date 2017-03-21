374be91a330ba97b5f05a85e890b5352cad0623a
Chris Eady <chris.eady1@gmail.com>
2017-03-21 16:40:51 -0500

    Add save game functionality.

--- lib/hangerman/game.rb
+++ lib/hangerman/game.rb
@@ -12,17 +12,56 @@ class Game
     display_board
 
     loop do
-      letter = ''
+      input = ''
       loop do
-        letter = @player.guess_letter
-        break if valid_guess?(letter)
+        input = @player.guess_letter
+        break if valid_guess?(input) && save_or_load_game(input)
       end
 
-      @letters_guessed << letter
-      @turns -= 1 if !correct_guess?(letter)
+      unless input =~ /^SAVE$|^LOAD$/
+        @letters_guessed << input
+        @turns -= 1 if !correct_guess?(input)
+      end
       display_board
       break if over?
     end
+  end
+
+  def self.save(game)
+    # Prompt player to choose saved game name.
+    puts "Name your saved game:"
+    name = $stdin.gets.chomp
+
+    # Create saved_games folder if it doesn't exits.
+    Dir.mkdir('bin/saved_games') if !Dir.exists?('bin/saved_games')
+
+    # Save the game.
+    File.open("bin/saved_games/#{name}", 'w') { |f| f.write(Marshal.dump(game)) }
+
+    puts "Game '#{name}' saved!"
+    sleep(1)
+  end
+
+  def self.load
+    puts "Pick saved game to load:"
+
+    # List saved games, ignoring '.' & '..' files.
+    saved_games = Dir.entries('bin/saved_games').select { |f| !/^\.+$/.match(f) }
+    saved_games.each_with_index do |filename, index|
+     puts "[#{ index + 1 }] #{filename}"
+   end
+
+   # Prompt player to choose game to load.
+   puts "\nChoose game to load:"
+   puts ">> "
+   game_choice = $stdin.gets.chomp.to_i
+   saved_game_filename = "bin/saved_games/#{saved_games[game_choice - 1]}"
+
+   # Load game.
+   saved_game = File.read(saved_game_filename)
+   puts ("'#{saved_games[game_choice - 1]}' loaded!")
+   sleep(1)
+   Marshal.load(saved_game)
   end
 
   private
@@ -53,12 +92,17 @@ class Game
     @word.include?(letter)
   end
 
-  def valid_guess?(letter)
-    if @letters_guessed.include?(letter)
-      puts "Invalid input! You already guessed '#{letter}'."
-      return false
+  def valid_guess?(input)
+    if input =~ /^[A-Za-z]$|^SAVE$|^LOAD$/i
+      if @letters_guessed.include?(input)
+        puts "Invalid input! You already guessed '#{input}'."
+        return false
+      else
+        return true
+      end
     end
-    return true
+    puts "Invalid input!"
+    return false
   end
 
   def word_guessed?
@@ -82,6 +126,16 @@ class Game
 
   def clear
     system "clear" or system "cls"
+  end
+
+  def save_or_load_game(input)
+    if input == 'SAVE'
+      Game.save(self)
+    elsif input == 'LOAD'
+      game = Game.load
+      game.play
+    end
+    true
   end
 
 end
--- lib/hangerman/player.rb
+++ lib/hangerman/player.rb
@@ -1,20 +1,20 @@
 class Player
 
   def guess_letter
-    input = ''
-    loop do
+    # input = ''
+    # loop do
       puts "Pick a letter:"
       print ">> "
       input = $stdin.gets.chomp
-      break if valid_letter?(input)
-    end
+      # break if valid_letter?(input)
+    # end
     input.upcase
   end
 
   private
 
   def valid_letter?(input)
-    if input =~ /^[A-Za-z]$/
+    if input =~ /^[A-Z]$|^save$|^load$|/i
       true
     else
       puts "Invalid input!"
